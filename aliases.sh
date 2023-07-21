f7886eb88faf509438f61ed3321bbbbe04d70dee
arekbee-macos <arekbee@MacBook-Pro-arekbee-macos.local>
2023-07-21 09:34:23 +0200

    update

--- aliases.sh
+++ aliases.sh
@@ -8,7 +8,15 @@ mkcd () {
 alias g=git 
 alias d=docker
 alias d_pune='docker system prune --all -volumes'
+
+
 alias k=kubectl
+alias k_n="kubectl config set-context --current --namespace"
+export K_DRY="--dry-run=client -o yaml"
+export K_NOW="--force --grace-period=0"
+
+
+
 alias a=ansible
 alias m=minikube
 alias m_s='minikube start --alsologtostderr -v=7'
