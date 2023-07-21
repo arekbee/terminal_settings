d4dbef2af1eca34992ede2ec391112d73e612c8c
arekbee <961862+arekbee@users.noreply.github.com>
2023-07-21 19:11:28 +0200

    update

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
 # terminal_settings
 
+## Clone this repo
+```bash
+git clone https://github.com/arekbee/terminal_settings.git
+```
 
+## Add those lines to `.bashrc` file 
 
 ```bash
-if [ -f ~/terminal_settings/.env ]; then
-  export $(cat ~/terminal_settings/.env | xargs)
+
+if [ -f ~/terminal_settings/bash.sh ]; then
+    . ~/terminal_settings/bash.sh
 fi
 
-if [ -f ~/terminal_settings/.bash_aliases ]; then
-    . ~/terminal_settings/.bash_aliases
+if [ -f ~/terminal_settings/env ]; then
+  export $(cat ~/terminal_settings/env | xargs)
 fi
+
 ```
--- bash.sh
+++ bash.sh
@@ -1,29 +1,46 @@
 
 
-mkcd () {
+mdcd () {
     mkdir -p "$1"
     cd "$1"
+    pwd
 }
 
-alias g=git 
+
+# ls
+alias l='ls -CF'
+alias ll='ls -alF'
+alias la='ls -A'
+
+
+# git
+alias g=git
+
+#python
+alias p=python3
+
+
+# docker
 alias d=docker
 alias d_pune='docker system prune --all -volumes'
 
 
+# k8s
 alias k=kubectl
 alias k_n="kubectl config set-context --current --namespace"
 export K_DRY="--dry-run=client -o yaml"
 export K_NOW="--force --grace-period=0"
-
-
-
-alias a=ansible
 alias m=minikube
 alias m_s='minikube start --alsologtostderr -v=7'
+
+
+# ansible
+alias a=ansible
+
+# terraform
 alias tf=terraform
 alias tf_graph='terraform graph | dot -o tf_graph.png -Tpng'
 alias tf_docs='terraform-docs markdown . >> _README.md'
 alias tg=terragrunt
-alias p=python3
-
+export TF_LOG_PATH=./log_$(date +"%Y%m%d_%H%M%S").txt
 
