08c9da6994bb020d619ecbabc57dcf0d5eca64c9
arekbee <961862+arekbee@users.noreply.github.com>
2023-07-24 17:52:17 +0200

    commaneds

--- bash.sh
+++ bash.sh
@@ -1,10 +1,17 @@
+#!/bin/bash
 
 
+# files
 mdcd () {
     mkdir -p "$@"
     cd "$@"
     pwd
 }
+
+alias diskspace="du -S | sort -n -r |more"
+alias folders='du -h --max-depth=1'
+alias tree='tree -CAhF --dirsfirst'
+alias treed='tree -CAFd'
 
 
 # ls
@@ -13,7 +20,7 @@ alias ll='ls -alHF'
 alias la='ls -A'
 alias ..='cd ..'  
 alias ....='cd ../..'
-
+alias rmd='/bin/rm  --recursive --force --verbose '
 
 # curl
 alias c="curl -L -k --retry-connrefused --retry 7 --retry-max-time 10 --verbose"
@@ -23,7 +30,7 @@ alias g=git
 
 #python
 alias p=python3
-
+alias p_server="python3 -m http.server"
 
 # docker
 alias d=docker
@@ -36,11 +43,26 @@ alias k_n="kubectl config set-context --current --namespace"
 alias k_a="kubectl apply -f"
 alias k_d="kubectl destroy -f"
 
+alias k_all="kubectl get all -A --show-labels"
+alias k_tail="tail "
+
+
 export K_DRY="--dry-run=client -o yaml"
 export K_NOW="--force --grace-period 0"
+export K_NSKS='-n kube-system'
 
 source <(kubectl completion bash)
 complete -F __start_kubectl k
+
+kns ()
+{
+[ $# -eq 0 ] && kubectl config get-contexts | grep --color=auto '^\*' | awk '{print $5}' || kubectl config set-context $(kubectl config current-context) --namespace=$1
+}
+
+alias kctx="kubectx"
+
+
+# minikube
 
 alias m=minikube
 alias m_s='minikube start --alsologtostderr -v=7'
@@ -71,4 +93,12 @@ fi
 
 #  Find Memory-Hungry Processes 
 alias ram='while read command percent rss; do if [[ "${command}" != "COMMAND" ]]; then rss="$(bc <<< "scale=2;${rss}/1024")"; fi; printf "%-26s%-8s%s\n" "${command}" "${percent}" "${rss}"; done < <(ps -A --sort -rss -o comm,pmem,rss | head -n 11)'
-alias ports='nmap localhost'  
+
+# Show open ports
+alias openports='netstat -nape --inet'
+alias openports2='nmap localhost'
+
+# Show current network connections to the server
+alias ipview="netstat -anpl | grep :80 | awk {'print \$5'} | cut -d\":\" -f1 | sort | uniq -c | sort -n | sed -e 's/^ *//' -e 's/ *\$//'"
+
+
---
+++ vimrc
@@ -0,0 +1,12 @@
+syntax enable
+set tabstop=2 
+set nu ts=2 sw=2 expandtab ruler
+set backspace=indent,eol,start
+set cursorline
+set showcmd	
+set showmatch
+set hlsearch
+set ignorecase
+set softtabstop=2 
+set expandtab
+filetype indent on
